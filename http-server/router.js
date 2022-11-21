417ff6a7c0b50963d65ec1e889b2e6fbdfd2553f
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-11-21 10:40:23 +0500

    Handling form data

--- http-server/router.js
+++ http-server/router.js
@@ -1,8 +1,8 @@
-function route(pathname, handler, request, response) {
+function route(pathname, handler, request, response, postData) {
   console.log(`Route for path requested: ${pathname}`);
 
   if (typeof handler[pathname] === "function") {
-    return handler[pathname](request, response);
+    return handler[pathname](request, response, postData);
   } else {
     console.log(`No Method found for ${pathname}`);
     return null;
--- http-server/start.js
+++ http-server/start.js
@@ -4,7 +4,22 @@ var url = require("url");
 function start(route, handler) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
+    var content;
+    var postData = "";
+    request.setEncoding("utf8");
+    if (request.method === "POST") {
+      request.addListener("data", function (chunk) {
+        postData += chunk;
+      });
+      request.addListener("end", function () {
+        content = route(pathname, handler, request, response, postData);
+      });
+    } else {
+      //content = route(pathname, handler, response);
+    }
+
     var content = route(pathname, handler, request, response);
+
     if (!content) {
       response.writeHead(404, { "Content-Type": "text/plain" });
       response.write("404 not found");
