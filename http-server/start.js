959a5483289943da681c5fcb77fb6af9f539eeba
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-11-21 10:00:01 +0500

    minor: refactoring

--- http-server/start.js
+++ http-server/start.js
@@ -2,13 +2,13 @@ var http = require("http");
 var url = require("url");
 
 function start(route, handler) {
-  function onRequest(req, res) {
-    var pathname = url.parse(req.url).pathname;
-    var content = route(pathname, handler, req, res);
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    var content = route(pathname, handler, request, response);
     if (!content) {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.write("404 not found");
-      res.end();
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.write("404 not found");
+      response.end();
     }
   }
   // use port 1337 unless there exists a preconfigured port
