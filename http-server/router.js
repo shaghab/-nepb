4a3151573acfd4e42ff1b37c14d9e1e5285db674
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-11-23 00:06:17 +0500

    refactoring: variable name change

--- http-server/router.js
+++ http-server/router.js
@@ -1,9 +1,9 @@
-function route(pathname, handler, request, response, postData) {
+function route(pathname, handler, request, response, data) {
   console.log(`Route for path requested: ${pathname}`);
   var method = request.method;
 
   if (typeof handler[[pathname, method]] === "function") {
-    return handler[[pathname, method]](request, response, postData);
+    return handler[[pathname, method]](request, response, data);
   } else {
     console.log(`No Method found for ${pathname} ${method}`);
     return null;
